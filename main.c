3ef763cadc6295a5045c5a5ea0ac7bb48fbd7728
F1reFinger <180121847@aluno.unb.br>
2023-01-21 22:01:40 -0300

    Num foi

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ void pid_configura_constantes(double Kp_, double Ki_, double Kd_);
 void pid_atualiza_referencia(float referencia_);
 double pid_controle(double saida_medida);
 
-#define I2C_ADDR 0x68 // I2C endereço do modulo
+#define I2C_ADDR 0x27 // I2C endereço do modulo
 
 #define RESISTOR 4
 #define FAN 5
